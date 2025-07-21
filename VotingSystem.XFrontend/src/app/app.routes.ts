9cb667529a2bbea035d81f931580121aaa844447
Sujal Maharjan <suzallmhz@gmail.com>
2025-07-21 17:11:34 +0545

    Angular Components, services and guard creation INIT

--- VotingSystem.XFrontend/src/app/app.routes.ts
+++ VotingSystem.XFrontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home' 
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home,
+        canActivate: [authGuard],
+        title: "Home"
+    }
+];
